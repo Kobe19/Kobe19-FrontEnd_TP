d8015546a685d6aa7a25fb4f5a2af674c5933ba4
kobe19 <fguirol@gmail.com>
2021-03-10 14:10:31 +0100

    outlet commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first-component', component: HomeComponent },
+  { path: 'second-component', component: LoginComponent },
+  { path: 'third-component', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
