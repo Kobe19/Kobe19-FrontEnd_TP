1d35161344b2fbc534e621dcf695f064ca87a44a
Kobe19 <fguirol@gmail.com>
2021-03-27 23:59:09 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 
 
@@ -35,7 +36,9 @@ import {ReactiveFormsModule} from '@angular/forms';
     MatInputModule,
     MatButtonModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import {HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth$LoginParams } from '@backend/routes/auth/post.login';
+
 
 
 
@@ -9,16 +12,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor (private router: Router) {}
-
+  
+  errors: string[] = [];
   email = ''
   password = ''
   
+  constructor (private router: Router, private httpClient: HttpClient) {}
   async onClickHandler() {
     //window.alert(`email : '${this.email}', password : '${this.password}'`);
-    window.alert("No account found for this email");
+    //window.alert("No account found for this email");
     //await this.router.navigateByUrl('/profile');
+
+    try {
+      this.errors = [];
+      const body: Auth$LoginParams = { email: this.email, password: this.password };
+      await this.httpClient.post('/auth/login', body).toPromise();
+      await this.httpClient.post('/auth/login', { email: this.email, password: this.password }).toPromise();
+      await this.router.navigateByUrl('/profile');
+    } catch (err) {
+      if (err instanceof HttpErrorResponse) this.errors = err.error?.messages;
+  
+      if (!this.errors) {
+        this.errors = ['An unknown error has occured'];
+        throw err; // resume error propagation, so monitoring tool (if any) catches it
+      }
+    }
   }
 
- 
-}
+  
+  }
+
+  
